c6806e20ace41856302a5ad408a455439a834250
Luis <lvas248@gmail.com>
2022-08-31 17:36:32 -0400

    Set routes in DisplaySection Comp

--- src/DisplaySection.js
+++ src/DisplaySection.js
@@ -3,12 +3,36 @@ import Suggest from "./Suggest.js"
 import Cart from "./Cart"
 import ProduceDisplayPage from "./ProduceDisplayPage"
 import WrapUp from "./WrapUp.js"
+import { Switch, Route } from 'react-router-dom'
 
 function DisplaySection(){
 
     return (
         <div id="displaySection">
             Display Section
+            <Switch>
+
+                <Route exact path="/">
+                    <Shop />
+                </Route>
+
+                <Route path="/suggest">
+                    <Suggest />
+                </Route>
+
+                <Route path="/cart">
+                    <Cart />
+                </Route>
+
+                <Route path="/produce-display-page">
+                    <ProduceDisplayPage />
+                </Route>
+
+                <Route path="/wrap-up">
+                    <WrapUp />
+                </Route>
+
+            </Switch>
         </div>
     )
 }
