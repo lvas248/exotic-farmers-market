e0a3861038a13d4eb7c5c9a63d32d55c02e1093c
Luis <lvas248@gmail.com>
2022-08-31 19:01:50 -0400

    Add functionality to search and filter, update styling

---
+++ src/SearchFilter.js
@@ -0,0 +1,19 @@
+
+
+function SearchFilter ({type, handleTypeChange, search, handleSearchTextChange, alphabetizeClick, handleBtnClick }) {
+
+    return (
+    <form id="search">
+        <button id="btn" onClick={handleBtnClick}>{alphabetizeClick === true ? "A → Z" : "Z → A"}</button>
+        <input id="searchBar" type="text" placeholder="search fruits and veggies..." value={search} onChange={(e)=> handleSearchTextChange(e.target.value)} />
+        <label>Filter by type:</label>
+        <select value={type} onChange={handleTypeChange}>
+            <option value="">All</option>
+            <option value="fruit">Fruits</option>
+            <option value="veggie">Veggies</option>
+        </select>
+    </form>
+    )
+}
+
+export default SearchFilter
