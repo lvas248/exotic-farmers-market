fad97b569a7e7e4a4eb207a7a35a61953eed0140
Luis <lvas248@gmail.com>
2022-08-31 17:25:54 -0400

    Update Nav with links

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"
 
 function Nav(){
 
     return (
         <nav>
-            Nav Page
+            <Link style={{textDecoration:"none"}} to="/"><h3>Shop</h3></Link>
+            <Link style={{textDecoration:"none"}} to="/suggest"><h3>Suggest a Fruit</h3></Link>
+            <Link style={{textDecoration:"none"}} to="/cart"><h3>Cart(0)</h3></Link>
         </nav>
     )
 }
