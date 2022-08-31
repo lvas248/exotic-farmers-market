5804e3d9f0586e75336db99f7a5f7bf66cc4c0c3
Luis <lvas248@gmail.com>
2022-08-31 17:53:53 -0400

    Update Shop Comp, fetch data, render ProduceCard Comps

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,26 @@
-import SearchFilter from "./SearchFilter.js"
+// import SearchFilter from "./SearchFilter.js"
 import ProduceCard from "./ProduceCard.js"
+import React, { useState, useEffect } from 'react'
 
 function Shop(){
+
+    //Initialize State -- ProduceList
+    const[ produceList, setProduceList ] = useState([])
+    //Get request - set state to data
+    useEffect(()=>{
+        fetch('http://localhost:8000/produce')
+        .then(res => res.json())
+        .then(data => setProduceList(data))
+    },[])
+
+
+    const renderProduce = produceList.map( produceObj=>{
+        return <ProduceCard key={produceObj.id} produceObj={produceObj}/>
+    })
     return (
         <div id="shop">
             Shop
+            {renderProduce}
         </div>
     )
 }
