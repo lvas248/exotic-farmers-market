7722b9d15e91ce16df221c9ef1e142c8b05a470e
Luis <lvas248@gmail.com>
2022-08-31 19:23:24 -0400

    Add functionality to click => display in ProduceDisplayPage comp

--- src/ProduceCard.js
+++ src/ProduceCard.js
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
 
-function ProduceCard({produceObj}){
+function ProduceCard({produceObj, handleSelectionClick}){
     return (
-        <Link style={{textDecoration:"none"}} to={"/produce-display-page"}>
-            <div className="produceCard" onClick={()=> console.log(produceObj)}>
+        <Link style={{textDecoration:"none"}} to={`/produce-display-page/${produceObj.name}`}>
+            <div id="produceCard" onClick={()=> handleSelectionClick(produceObj)}>
                 <img src={produceObj.image} alt={produceObj.name}/>
                 <h4>{produceObj.name}</h4>
                 <p>${produceObj.price} per pound</p>
--- src/ProduceDisplayPage.js
+++ src/ProduceDisplayPage.js
@@ -1,10 +1,17 @@
 
-
-function ProduceDisplayPage(){
+function ProduceDisplayPage({selectedProduceItem}){
 
     return (
         <div id="produceDisplayPage">
-            ProduceDisplayPage
+            <h1>{selectedProduceItem.name}</h1>
+            <img src={selectedProduceItem.image} alt={selectedProduceItem.name}/>
+            <p id="desc">{selectedProduceItem.description}</p>
+            <h5>${selectedProduceItem.price} per pound</h5>
+            <form onSubmit={console.log("submit")}>
+                <label>Order in  lbs: </label>
+                <input type="text" placeholder="qty" onChange={console.log("handleChange")}/>
+                <button type="submit">Add to Cart</button>
+            </form>
         </div>
     )
 }
