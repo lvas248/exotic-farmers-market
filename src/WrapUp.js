1dc9e2420f8e74369232df85f245daf5da15a731
Luis <lvas248@gmail.com>
2022-09-01 13:26:01 -0400

    create controlled form

--- src/WrapUp.js
+++ src/WrapUp.js
@@ -1,16 +1,17 @@
-
+import React, { useState } from 'react'
 
 function WrapUp({cart}){
     
+    const [ userInfo, setUserInfo ] = useState({name:"", pickUpDate: "", pickUpTime: "", notes: "", cart: cart, totalCost:""})
 
-
+  
     return(
         <div id="wrapUp">
             <h1>Pick Up Details</h1>
             <form type="submit">
                 <div>
                     <label>Name: </label>
-                    <input type="text" ></input>
+                    <input type="text" value={userInfo.name} onChange={e => setUserInfo({...userInfo, name: e.target.value})}></input>
                 </div>
                 <div>
                     <label>Pick Up Date</label>
@@ -20,14 +21,14 @@ function WrapUp({cart}){
                 </div>
                 <div>
                 <label>Pick up time</label>  
-                    <select>
+                    <select value={userInfo.pickUpTime} onChange={e => setUserInfo({...userInfo, pickUpTime: e.target.value})}>
                         <option>Morning: 10a-12p</option>
                         <option>Evening: 4p-6p</option>
                     </select>
                 </div>
                 <div>
                     <label>Notes: </label>
-                    <input type="text" style={{width:"250px"}}></input>
+                    <input type="text" style={{width:"250px"}} value={userInfo.notes} onChange={e => setUserInfo({...userInfo, notes: e.target.value})}></input>
                 </div>
                 <button type="submit">Submit</button>
             </form>
