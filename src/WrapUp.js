f1cb846d9e831945a72aae09bd92526abeab54a7
Luis <lvas248@gmail.com>
2022-09-01 13:08:12 -0400

    create form in WrapUp comp

--- src/WrapUp.js
+++ src/WrapUp.js
@@ -1,10 +1,37 @@
 
 
-function WrapUp(){
+function WrapUp({cart}){
+    
+
 
     return(
         <div id="wrapUp">
-            Wrap up
+            <h1>Pick Up Details</h1>
+            <form type="submit">
+                <div>
+                    <label>Name: </label>
+                    <input type="text" ></input>
+                </div>
+                <div>
+                    <label>Pick Up Date</label>
+                    <select>
+                        {/* {renderOptions}                         */}
+                    </select>
+                </div>
+                <div>
+                <label>Pick up time</label>  
+                    <select>
+                        <option>Morning: 10a-12p</option>
+                        <option>Evening: 4p-6p</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Notes: </label>
+                    <input type="text" style={{width:"250px"}}></input>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+          
         </div>
     )
 }
