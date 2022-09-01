c08fe1c3361cc82f5faa230958c64a5a32c185e7
Luis <lvas248@gmail.com>
2022-08-31 23:27:31 -0400

    Fetch data, set State in Suggest comp

--- src/Suggest.js
+++ src/Suggest.js
@@ -1,6 +1,17 @@
 // import SuggestedProduceCard from "./SuggestedProduceCard"
+import React, { useEffect, useState } from 'react'
 
 function Suggest(){
+
+    //Fetch Suggested Fruit Data, set state
+    const [ suggestedFruitList, setSuggestedFruitList ] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:3000/suggestions')
+        .then(res => res.json())
+        .then( data => setSuggestedFruitList(data))
+      }, []) 
+
+      
 
     return (
         <div id="suggest">
