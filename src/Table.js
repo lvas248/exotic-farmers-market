6f1d97975c8cfdda90ab68a1fa5d2ba3e3fd80e4
Luis <lvas248@gmail.com>
2022-09-06 16:46:25 -0400

    Create Table component

---
+++ src/Table.js
@@ -0,0 +1,47 @@
+import CartItem from "./CartItem"
+
+function Table({updateCart, cart}){
+    
+
+    const totalWeight = cart.reduce((total, item) => {
+        return total + parseInt(item.weight)
+    }, 0)
+
+    const totalCost = cart.reduce((total, item)=>{
+        return total + (item.weight * item.price)
+    }, 0)
+
+    const renderLineItems = cart.map( item =>{
+        return <CartItem key={item.name} updateCart={updateCart} lineItem={item}/>
+    })
+    
+    return(
+    <table id="table">
+        <thead id="head">
+            <tr>
+                <th className="fruitName">Name</th>
+                <th>Cost / lb</th>
+                <th>Weight</th>
+                <th>Total</th> 
+            </tr>
+        </thead>
+        <tbody>
+            {renderLineItems}
+            <tr>
+                <th></th>
+                <th></th>
+                <th className="total">Total weight</th>
+                <th className="total">Total Cost</th>
+            </tr>  
+            <tr>
+                <td></td>
+                <td></td>
+                <td>{totalWeight} lbs</td>
+                <td>${totalCost}.00</td>
+            </tr>                       
+        </tbody>
+    </table>
+    )
+}
+
+export default Table
