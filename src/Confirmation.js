cb59611c15b3d7380d4138d57a6720b64cbb1568
Luis <lvas248@gmail.com>
2022-09-06 18:04:42 -0400

    Create Confirmation component

---
+++ src/Confirmation.js
@@ -0,0 +1,18 @@
+
+
+function Confirmation({orderDetails}){
+    console.log(orderDetails)
+    return (
+    <div id="confirmation">
+        <h1>Confirmation Details</h1>
+        <p>Confirmation number: <strong>{orderDetails.id}</strong></p>
+        <p>Name: <strong>{orderDetails.name}</strong></p>
+        <p>Pick up date: <strong>{orderDetails.pickUpDate}</strong></p>
+        <p>Pick up time: <strong>{orderDetails.pickUpTime}</strong></p>
+        <p>Price: <strong>${orderDetails.totalCost}.00</strong></p>
+        <p>Notes: {orderDetails.notes}</p>
+    </div>
+    )
+}
+
+export default Confirmation
