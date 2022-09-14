6c43d41f7b7fc0c454fe2900b4e360781d6f9534
Luis <lvas248@gmail.com>
2022-09-14 15:24:21 -0400

    test

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,7 @@ function Nav(){
 
     return (
         <nav>
-            <Link style={{textDecoration:"none"}} to="/"><h3>Shop</h3></Link>
+            <Link style={{textDecoration:"none"}} to="/"><h3>SHOP</h3></Link>
             <Link style={{textDecoration:"none"}} to="/add-produce"><h3>Add Produce</h3></Link>
             <Link style={{textDecoration:"none"}} to="/cart"><h3>Cart</h3></Link>
         </nav>
