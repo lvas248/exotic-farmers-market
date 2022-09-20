e2e66afb5550cd288fdfeebc27183fb632d414d2
Luis <lvas248@gmail.com>
2022-09-20 14:17:12 -0400

    update

--- src/CartItem.js
+++ src/CartItem.js
@@ -30,7 +30,7 @@ function CartItem({lineItem, updateCart, deleteFromCart}){
                 <td>{btnClick? <input placeholder={lineItem.weight} value={updatedWeight} onChange={handleChange}></input>: lineItem.weight} lbs</td>
                 <td>${lineItem.weight * lineItem.price}.00</td>
                 <td>{btnClick ? <button onClick={handleUpdate}>Update</button> : <button onClick={handleEdit}>Edit</button>}</td>
-                <td><button onClick={()=>deleteFromCart(lineItem)}>Delete</button></td>
+                {btnClick ? <td><button onClick={()=>deleteFromCart(lineItem)}>Delete</button></td> : null}
 
             </tr>
         </>
